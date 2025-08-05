4f04c056d8c0bcfb88237c05a75ef2eb8e44adf6
Juan David Mayorga <130000511+JuandaMT@users.noreply.github.com>
2025-08-05 21:24:45 +0200

    changed calculator container

--- calculadora/src/App.tsx
+++ calculadora/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<div className='calculator'>
+			{/* 2. La pantalla es el primer hijo directo */}
+			<div className='display'>0</div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+			{/* 3. TODOS los botones son hijos directos del contenedor .calculator */}
+			<button className='btn btn--function'>AC</button>
+			<button className='btn btn--function'>+/-</button>
+			<button className='btn btn--function'>%</button>
+			<button className='btn btn--operator'>÷</button>
+
+			<button className='btn btn--number'>7</button>
+			<button className='btn btn--number'>8</button>
+			<button className='btn btn--number'>9</button>
+			<button className='btn btn--operator'>×</button>
+
+			<button className='btn btn--number'>4</button>
+			<button className='btn btn--number'>5</button>
+			<button className='btn btn--number'>6</button>
+			<button className='btn btn--operator'>-</button>
+
+			<button className='btn btn--number'>1</button>
+			<button className='btn btn--number'>2</button>
+			<button className='btn btn--number'>3</button>
+			<button className='btn btn--operator'>+</button>
+
+			{/* El botón cero tiene una clase extra para ocupar dos columnas */}
+			<button className='btn btn--number btn--zero'>0</button>
+			<button className='btn btn--number'>.</button>
+			<button className='btn btn--operator'>=</button>
+		</div>
+	)
 }
 
 export default App
